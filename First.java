8314cac699179d7d5bc7b79c41af9cb0adeb7fec
kpoorna5750-jpg <kpoorna5750@gmail.com>
2026-01-31 14:35:25 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class First{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the radius of circle:");
+        int r=sc.nextInt();
+        double Area=3.14*r*r;
+        System.out.println("Area:"+Area);
+    }
+}    
+
+        
---
+++ Test.java
@@ -0,0 +1,8 @@
+class Test{
+    public static void main(String[]args){
+        System.out.print("Java");
+        System.out.println("Course");
+        System.out.println("Platform");
+        
+    }
+}
