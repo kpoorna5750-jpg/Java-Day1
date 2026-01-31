fa91e083dc4cfb2f40384cb29b7491e9bae11d49
kpoorna5750-jpg <kpoorna5750@gmail.com>
2026-01-31 15:17:48 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,11 @@
+class Factorial{
+    public static void main(String[] args){
+        int num=6;
+        long factorial=1;
+        for(int i=1;i<=num;i++){
+            factorial*=i;
+        }
+        System.out.println("factorial of given number is:"+factorial);
+
+    }
+}
